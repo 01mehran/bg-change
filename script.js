8fad4e68d5836a7a54c022a739601ad6ee33762e
01mehran <132054519+01mehran@users.noreply.github.com>
2024-02-11 01:22:36 +0330

    created the objectArray

---
+++ script.js
@@ -0,0 +1,48 @@
+const colors = [
+  {
+    name: "DEFULT",
+    color: "#000",
+    textColor: "yellow",
+  },
+  {
+    name: "aqua",
+    color: "aqua",
+    textColor: "brown",
+  },
+  {
+    name: "red",
+    color: "#FF0000",
+    textColor: "#fff",
+  },
+  {
+    name: "green",
+    color: "green",
+    textColor: "white",
+  },
+  {
+    name: "purple",
+    color: "indigo",
+    textColor: "yellow",
+  },
+  {
+    name: "light-pink",
+    color: "#B0171F",
+    textColor: "#FFFF00",
+  },
+  {
+    name: "lightgreen",
+    color: "lightgreen",
+    textColor: "#B0171F",
+  },
+  {
+    name: "blue",
+    color: "blue",
+    textColor: "orange",
+  },
+  {
+    name: "brown",
+    color: "brown",
+    textColor: "aqua",
+  },
+];
+
